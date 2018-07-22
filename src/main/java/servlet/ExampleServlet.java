b379e259bb6b4c79b278663e2dac6c53ef4edade
L.Zhang <ipiszhang@hotmail.com>
2018-07-22 23:43:03 +0800

    MOD-调试程序

--- src/main/java/servlet/ExampleServlet.java
+++ src/main/java/servlet/ExampleServlet.java
@@ -9,18 +9,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
 
 @WebServlet(urlPatterns = "/example", name = "example")
 public class ExampleServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		Writer writer = resp.getWriter();
-		writer.write("do get");
+		this.doPost(req, resp);
 	}
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		Writer writer = resp.getWriter();
-		writer.write("do post");
+		ObjectMapper mapper = new ObjectMapper();
+		Map<String, Object> map = new HashMap<>();
+		map.put("key", "value");
+		map.put("one", "icy");
+		mapper.writeValueAsString(map);
+		writer.write(mapper.writeValueAsString(map));
 	}
 }
